56179bb1ed2525b2b4a95b6232f181bf10744fc6
Manato1fg <manato1fg@gmail.com>
2020-05-05 14:40:16 +0900

    Auto update

--- app/automator.sh
+++ app/automator.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
+
+#
+# アプリの内部で使われているローンチとアップデート用のスクリプト
+#
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 DIR="toshinApp"
 
-if [ ! -d $DIR ];then
-	osascript -e 'display notification "初回起動のため必要ファイルをダウンロードしています。しばらくお待ちください。(約40MB)" with title "東進受講.app"'
-	mkdir $DIR
+download() {
+    mkdir $DIR
 	cd $DIR
 	curl -OL https://toshin4mac.netlify.app/install/app.zip
     ls .
 	unzip ./app.zip
 	chmod 777 ./app/app.sh
 	rm ./app.zip
+    touch "./app/version.txt"
+    VERSION=`curl https://toshin4mac.netlify.app/install/version.txt`
+    echo $VERSION >> "./app/version.txt"
 	cd ./..
+}
+
+if [ ! -d $DIR ];then
+	osascript -e 'display notification "初回起動のため必要ファイルをダウンロードしています。しばらくお待ちください。(約40MB)" with title "東進受講.app"'
+	download
     osascript -e 'display notification "ダウンロードが完了しました。" with title "東進受講.app"'
+else 
+    CURRENT_VERSION=`cat $DIR/app/version.txt`
+    VERSION=`curl https://toshin4mac.netlify.app/install/version.txt`
+    echo $CURRENT_VERSION
+    echo $VERSION
+    if [ $CURRENT_VERSION -lt $VERSION ]; then
+        osascript -e 'display notification "最新バージョンにアップデートしています。しばらくお待ちください。" with title "東進受講.app"'
+        rm -rf $DIR
+        download
+        osascript -e 'display notification "アップデートが完了しました。" with title "東進受講.app"'
+    fi
 fi
+
 bash --login $DIR/app/app.sh 
